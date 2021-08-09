5fc5accfe47f1ac4766fce5eebc93b9a0b2132cd
Hugo Sobral <hugosobral10@gmail.com>
2021-08-09 00:02:17 -0300

    Improve table settings on App
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import './App.css';
 import { Box } from "@chakra-ui/react"
+import { useState, useEffect } from 'react';
 
 
 function App() {
-  const renderCol = () => {
-    let numberOfCols = 15;
-    let cols = [];
-    for (let i=0; i<numberOfCols; i++){
-      cols.push(i);
-    }
+  const [numberOfCols, setNumberOfCols] = useState(0);
+  
+  useEffect(() => {
+    setNumberOfCols(20);
+  }, [])
 
-    console.log(cols);
-  } 
+  const renderCol = (row) => {
+    let cols = [];
+    for (let i=0; i<numberOfCols; i++) cols.push(i);
+
+    return cols.map((column) => (
+      <Box 
+        className="pixel"
+        border="1px"
+        borderColor="gray.200"
+        onClick={console.log("alisa meu pelo")}
+        id={`row${row}-col${column}`}
+        key={`row${row}-col${column}`}
+      >
+      </Box>
+    ));
+  }
+
+  const renderTable = () => {
+    let rows = []
+    for (let i=0; i<numberOfCols; i++) rows.push(i);
+    return rows.map((row) => (
+      <Box
+        className="table"
+        key={row}
+      >
+        {renderCol(row)}
+      </Box>
+    ))
+  }
 
   return (
     <div className="App">
-      <h1>oi</h1>
-      <button onClick={renderCol}>alisa meu pelo</button>
-      <Box 
-        className="pixel"
-        border="2px"
-        onClick={renderCol}
-      >
-      </Box>
+      <h1>Romero Britto Generator</h1>
+      <div className="alignBoard">
+        {renderTable()}
+      </div>
+      
     </div>
   );
 }
