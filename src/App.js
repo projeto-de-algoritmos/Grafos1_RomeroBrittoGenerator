b8e78c168afbbe2473cb3c73ddb87b54b1d1899c
Leonardo Gomes <leonardodasigomes@gmail.com>
2021-08-09 23:00:57 -0300

    Adjust button
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,76 +1,82 @@
-import './App.css';
-import { Box } from "@chakra-ui/react"
-import { useEffect, useState } from 'react';
-import Graph from './structures/Graph';
-
+import "./App.css";
+import { Box } from "@chakra-ui/react";
+import { Button, Switch } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import Graph from "./structures/Graph";
 
 function App() {
   const numberOfCols = 8;
   const [clearBoard, setClearBoard] = useState(false);
   const [color, setColor] = useState("#654321");
-  const [makeWall, setMakeWall] = useState(false);
+  // const [makeWall, setMakeWall] = useState(false);
 
   useEffect(() => {
     if (clearBoard) setColor("white");
     else setColor("#654321");
   }, [clearBoard]);
 
-  useEffect(() => {
-    if (makeWall) setColor("black");
-    else setColor("#654321");
-  }, [makeWall]);
+  // useEffect(() => {
+  //   if (makeWall) setColor("black");
+  //   else setColor("#654321");
+  // }, [makeWall]);
 
   const renderCol = (row, graph) => {
     let cols = [];
-    for (let i=0; i<numberOfCols; i++) cols.push(i);
+    for (let i = 0; i < numberOfCols; i++) cols.push(i);
 
     return cols.map((column) => (
-      <Box 
+      <Box
         className="pixel"
         border="1px"
         borderColor="gray.200"
         id={`row${row}-col${column}`}
         key={`row${row}-col${column}`}
         onClick={() => {
-          let id = `row${row}-col${column}`;
+          // let id = `row${row}-col${column}`;
           // graph.dfs(id, color)
           graph.romeroBritto();
         }}
-      >
-      </Box>
+      ></Box>
     ));
-  }
+  };
 
   const renderTable = () => {
     const graph = new Graph(numberOfCols, numberOfCols);
     graph.buildGraph();
 
     let rows = [];
-    for (let i=0; i<numberOfCols; i++) rows.push(i);
+    for (let i = 0; i < numberOfCols; i++) rows.push(i);
     return rows.map((row) => (
-        <Box
-          className="table"
-          key={row}
-        >
-          {renderCol(row, graph)}
-        </Box>
-    ))
-  }
+      <Box className="table" key={row}>
+        {renderCol(row, graph)}
+      </Box>
+    ));
+  };
 
   return (
     <div className="App">
-      <h1>Romero Britto Generator</h1>
-      <div className="alignBoard">
-        {renderTable()}
+      <div className="content">
+        <h1>Romero Britto Generator</h1>
+        <div className="alignBoard">{renderTable()}</div>
+        <div className="buttonsContent">
+          <div className="swicthContent">
+            {clearBoard && <p className="activateSwitch">Pintar quadro</p>}
+            {!clearBoard && <p className="deactivateSwitch">Pintar quadro</p>}
+            <Switch
+              color="primary"
+              onChange={() => setClearBoard(!clearBoard)}
+            />
+            {!clearBoard && <p className="activateSwitch">Borracha</p>}
+            {clearBoard && <p className="deactivateSwitch">Borracha</p>}
+          </div>
+          <Button variant="contained" color="primary">
+            Apagar tudo
+          </Button>
+          <Button variant="contained" color="primary">
+            Romero Britto
+          </Button>
+        </div>
       </div>
-      <button
-        onClick={() => setClearBoard(!clearBoard)}
-      >
-        {clearBoard? 'Pintar quadro' : 'Apagar quadro'}
-      </button>
-      <button>
-        Pintar as paredes
-      </button>
     </div>
   );
 }
